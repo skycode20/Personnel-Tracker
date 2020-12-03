2238b89149944f95cdfde0ee34f10a7f6e1abe02
Skyler R <skyler.rencher@gmail.com>
2020-12-03 00:43:25 -0500

    added some commands to the seed db.

---
+++ db/personnelSeeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brick", "Shooter", 45, 23);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Floyd", "Mayweather", 100, 9);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andy", "Brandy", 33, 1);
+
+INSERT INTO role (title, salary, dept_id) VALUES ("Power Forward", 900000, 8);
+INSERT INTO role (title, salary, dept_id) VALUES ("Boxer", 700000, 30);
+INSERT INTO role (title, salary, dept_id) VALUES ("Chief Distiller", 250000, 1738);
+
+INSERT INTO department (dept_name) VALUES ("Basketball");
+INSERT INTO department (dept_name) VALUES ("Boxing");
+INSERT INTO department (dept_name) VALUES ("Spirits");
+
+
