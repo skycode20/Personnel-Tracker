2739faf54a45e06d1fbd580334c6d5203f423607
Skyler R <skyler.rencher@gmail.com>
2020-12-06 21:58:26 -0500

    changed the schema and seeds a little bit.

--- db/personnelSchema.sql
+++ db/personnelSchema.sql
@@ -4,31 +4,28 @@ CREATE DATABASE IF NOT EXISTS personnel_tracker_db;
 
 USE personnel_tracker_db;
 
-DROP TABLE IF EXISTS department;
-
-DROP TABLE IF EXISTS role;
-
-DROP TABLE IF EXISTS employee;
-
-CREATE TABLE department (
+CREATE TABLE IF NOT EXISTS department (
     id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE IF NOT EXISTS role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     dept_id INT NOT NULL,
     PRIMARY KEY (id),
+    FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
--- db/personnelSeeds.sql
+++ db/personnelSeeds.sql
@@ -1,13 +1,11 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brick", "Shooter", 45, 23);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Floyd", "Mayweather", 100, 9);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Andy", "Brandy", 33, 1);
+USE personnel_tracker_db;
 
-INSERT INTO role (title, salary, dept_id) VALUES ("Power Forward", 900000, 8);
-INSERT INTO role (title, salary, dept_id) VALUES ("Boxer", 700000, 30);
-INSERT INTO role (title, salary, dept_id) VALUES ("Chief Distiller", 250000, 1738);
+INSERT INTO department (dept_name) VALUES ("Basketball"), ("Boxing"), ("Spirits"), ("Marketing"), ("Product Development"), ("Fashion");
 
-INSERT INTO department (dept_name) VALUES ("Basketball");
-INSERT INTO department (dept_name) VALUES ("Boxing");
-INSERT INTO department (dept_name) VALUES ("Spirits");
+
+INSERT INTO role (title, salary, dept_id) VALUES ("Power Forward", "900000", "1"), ("Boxer", "700000", "2"), ("Chief Distiller", "250000", "3"), ("Director", "300000", "4"), ("Engineer", "120000", "5"), ("Designer", "90000", "6");
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Brick", "Shooter", "1", "2"), ("Floyd", "Mayweather", "2", "3"), ("Andy", "Brandy", "3", "4"), ("Ashanti", "Dior", "4", "5"), ("Melinda", "Ruiz", "5", "6"), ("Tyrone", "Jackson", "6", "1");
 
 
