d0e51bb991d77888ba4b98e8ffbfebf00940bdf1
Skyler R <skyler.rencher@gmail.com>
2020-12-19 00:57:04 -0500

    changed the viewemployees by department func to an operational code.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
   start();
@@ -25,64 +25,90 @@ connection.connect(function(err) {
 
 // function which prompts the user for what action they should take
 function start() {
-    inquirer
-      .prompt({
-        name: "userWhatToDo",
-        type: "list",
-        message: "What would you like to do?",
-        choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles", "Add Role", "Remove Role", "EXIT"]
-      })
-      .then(function(answer) {
-        // based on their answer, either call the bid or the post functions 
-        if (answer.userWhatToDo === "View All Employees") {
-          viewEmployeesAll();
-        }
-        else if(answer.userWhatToDo === "View All Employees By Department") {
-          viewEmployeesByDept();
-        } 
-        else if(answer.userWhatToDo === "View All Employees By Manager") {
-          viewEmployeesByManager();
-        } 
-        else if(answer.userWhatToDo === "Add Employee") {
-          addEmployee();
-        } 
-        else if(answer.userWhatToDo === "Remove Employee") {
-          removeEmployee();
-        } 
-        else if(answer.userWhatToDo === "Update Employee Role") {
-          updateEmployeeRole();
-        } 
-        else if(answer.userWhatToDo === "Update Employee Manager") {
-          updateEmployeeManager();
-        } 
-        else if(answer.userWhatToDo === "View All Roles") {
-          viewAllRoles();
-        } 
-        else if(answer.userWhatToDo === "Add Role") {
-          addRole();
-        } 
-        else if(answer.userWhatToDo === "Remove Role") {
-          removeRole();
-        } 
-        else if(answer.userWhatToDo === "EXIT") {
-          connection.end();
-        }
-      });
-  }
+  inquirer
+    .prompt({
+      name: "userWhatToDo",
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager", "View All Roles", "Add Role", "Remove Role", "EXIT"]
+    })
+    .then(function (answer) {
+      // based on their answer, either call the bid or the post functions 
+      if (answer.userWhatToDo === "View All Employees") {
+        viewEmployeesAll();
+      }
+      else if (answer.userWhatToDo === "View All Employees By Department") {
+        viewEmployeesByDept();
+      }
+      else if (answer.userWhatToDo === "View All Employees By Manager") {
+        viewEmployeesByManager();
+      }
+      else if (answer.userWhatToDo === "Add Employee") {
+        addEmployee();
+      }
+      else if (answer.userWhatToDo === "Remove Employee") {
+        removeEmployee();
+      }
+      else if (answer.userWhatToDo === "Update Employee Role") {
+        updateEmployeeRole();
+      }
+      else if (answer.userWhatToDo === "Update Employee Manager") {
+        updateEmployeeManager();
+      }
+      else if (answer.userWhatToDo === "View All Roles") {
+        viewAllRoles();
+      }
+      else if (answer.userWhatToDo === "Add Role") {
+        addRole();
+      }
+      else if (answer.userWhatToDo === "Remove Role") {
+        removeRole();
+      }
+      else if (answer.userWhatToDo === "EXIT") {
+        connection.end();
+      }
+    });
+}
 
-  function viewEmployeesAll(data) {
-    console.log("Selecting all employees...\n");
-    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.dept_id, department.dept_name, role.salary
+function viewEmployeesAll() {
+  console.log("Selecting all employees...\n");
+  connection.query(`SELECT employee.id, employee.first_name, employee.last_name, employee.role_id, employee.manager_id, role.title, role.dept_id, department.dept_name, role.salary
     FROM personnel_tracker_db.employee
     JOIN personnel_tracker_db.role
     ON personnel_tracker_db.employee.role_id=personnel_tracker_db.role.id
     JOIN personnel_tracker_db.department
-    ON personnel_tracker_db.role.dept_id=personnel_tracker_db.department.id`, data, function(err, res) {
-      if (err) throw err;
-      // Log all results of the SELECT statement
-      console.table(res);
-      start();
+    ON personnel_tracker_db.role.dept_id=personnel_tracker_db.department.id`, function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.table(res);
+    start();
     //   connection.end();
-    });
-  }
-  
+  });
+}
+
+function viewEmployeesByDept() {
+  console.log("Selecting employees by deparment...\n");
+  let userChoice = [];
+  connection.query("SELECT department.dept_name FROM personnel_tracker_db.department", function (err, res) {
+    if (err) throw err;
+    userChoice = res;
+    inquirer
+      .prompt([{
+        name: "selectDept",
+        type: "list",
+        message: "Which department would you like to select?",
+        choices: userChoice
+      }]).then(function (answer) {
+        connection.query(`SELECT employee.first_name, employee.last_name, department.dept_name AS department 
+          FROM employee JOIN role ON employee.role_id = role.id 
+          JOIN department ON role.dept_id = department.id 
+          ORDER BY employee.id`, answer.selectDept, function (err, res) {
+          if (err) throw err;
+          // Log all results of the SELECT statement
+          console.table(res);
+          start();
+          //   connection.end();
+        });
+      })
+  })
+}
